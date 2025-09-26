9ca3c463aeb1122e29904741ca211fa46d501b3d
Eliasld <area_causation872@simplelogin.fr>
2025-09-26 18:51:15 +0200

    Complete the tool, notifying the user how many official(s) + AUR related update(s) are available

--- system-update/update.py
+++ system-update/update.py
@@ -26,3 +26,20 @@ def check_aur_updates():
     result = run("yay -Qu")
     packages = result.stdout.strip().split("\n") if result.stdout else []
     return packages
+
+def main():
+    pacman_updates = check_pacman_updates()
+    aur_updates = check_aur_updates()
+    total_updates = len(pacman_updates) + len(aur_updates)
+
+    if total_updates == 0:
+        notify("System Update", "No updates available 🎉")
+        log("No updates available.")
+        return
+
+    update_msg = f"{len(pacman_updates)} official + {len(aur_updates)} AUR updates availble."
+    notify("System Update", update_msg)
+    log(update_msg)
+
+if __name__ == "__main__":
+    main()
